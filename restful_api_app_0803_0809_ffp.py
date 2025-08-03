954275ef21b95cda1afe71470dc423cbc5a2f301
malAKAI2858 <waltzer.juniors-0r@icloud.com>
2025-08-03 09:09:41 +0900

    Add PYTHON RESTful API接口开发

---
+++ restful_api_app_0803_0809_ffp.py
@@ -0,0 +1,13 @@
+# 代码生成时间: 2025-08-03 08:09:41
+{
+    "# 引入模块和库"
+    "import tkinter as tk"
+    "from tkinter import messagebox"
+    "from flask import Flask, request, jsonify"
+    ""
+    "RESTful API接口开发，使用Python和Tkinter框架"
+# 增强安全性
+    ""
+
+    # 创建Flask应用"
+    
